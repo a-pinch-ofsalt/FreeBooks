dc99f00036ede7bda1d02510e8b2a4a49d9f82a5
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-08-17 20:45:15 -0400

    Set port to 0.0.0.0

--- runner.py
+++ runner.py
@@ -8,7 +8,7 @@ def upload_epub():
     return jsonify({"message": "EPUB uploaded successfully!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
 
